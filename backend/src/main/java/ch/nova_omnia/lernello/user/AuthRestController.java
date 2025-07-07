0b5a971ba47d70b39cb86736610d186a1703b05f
deomiarn <deomicarnel@hotmail.com>
2025-07-07 17:51:23 +0200

    feat: add forgot password endpoint to handle password reset requests

--- backend/src/main/java/ch/nova_omnia/lernello/user/AuthRestController.java
+++ backend/src/main/java/ch/nova_omnia/lernello/user/AuthRestController.java
@@ -1,18 +1,20 @@
 package ch.nova_omnia.lernello.user;
 
+import ch.nova_omnia.lernello.user.dto.request.ForgotPasswordDTO;
+import ch.nova_omnia.lernello.user.dto.request.UserLoginDTO;
+import ch.nova_omnia.lernello.user.dto.response.LoggedInUserDTO;
+import ch.nova_omnia.lernello.user.mapper.UserLoginMapper;
+import ch.nova_omnia.lernello.user.model.User;
+import ch.nova_omnia.lernello.user.service.PasswordResetService;
+import ch.nova_omnia.lernello.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import ch.nova_omnia.lernello.user.dto.request.UserLoginDTO;
-import ch.nova_omnia.lernello.user.dto.response.LoggedInUserDTO;
-import ch.nova_omnia.lernello.user.mapper.UserLoginMapper;
-import ch.nova_omnia.lernello.user.model.User;
-import ch.nova_omnia.lernello.user.service.UserService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 
 /**
  * Controller for handling authentication requests.
@@ -23,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 @Validated
 public class AuthRestController {
     private final UserService userService;
+    private final PasswordResetService passwordResetService;
     private final UserLoginMapper userLoginMapper;
 
     /**
@@ -35,5 +38,13 @@ public class AuthRestController {
     public @Valid LoggedInUserDTO signin(@RequestBody @Valid UserLoginDTO userLoginDTO) {
         User authenticatedUser = userService.authenticate(userLoginDTO.username(), userLoginDTO.password());
         return userLoginMapper.toDTO(authenticatedUser);
+    }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<?> forgotPassword(@RequestBody @Valid ForgotPasswordDTO request) {
+        passwordResetService.processForgotPassword(request.getEmail());
+
+        // Always same response, no indication if user exists, for user-leakage prevention
+        return ResponseEntity.ok().build();
     }
 }
