4b959e7bdc97bdfaa097fd71d03ed8a7f588c346
deomiarn <deomicarnel@hotmail.com>
2025-07-07 17:56:29 +0200

    feat: add PasswordResetTokenRepository for managing password reset tokens

---
+++ backend/src/main/java/ch/nova_omnia/lernello/user/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,14 @@
+package ch.nova_omnia.lernello.user.repository;
+
+import ch.nova_omnia.lernello.user.model.PasswordResetToken;
+import ch.nova_omnia.lernello.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, UUID> {
+    Optional<PasswordResetToken> findByToken(String token);
+
+    void deleteAllByUser(User user); // optional für Token-Invalidierung
+}
